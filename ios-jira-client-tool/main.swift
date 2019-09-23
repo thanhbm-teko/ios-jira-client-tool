2a93de3bfb71bd9faf735a7be6c3b0ecdd7ec2eb
Nguyễn Xuân Tùng <tung.nx@teko.vn>
2019-09-23 17:56:57 +0700

    Initial Commit

---
+++ ios-jira-client-tool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ios-jira-client-tool
+//
+//  Created by Tung Nguyen on 9/23/19.
+//  Copyright © 2019 Tung Nguyen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
